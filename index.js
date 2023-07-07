a2c9222739966f12fe487afc41a73b9d36873b19
Juhani Viinanen <juhani.ilmari.viinanen@gmail.com>
2023-07-07 18:12:17 +0300

    3.6 done

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const app = express()
 
 app.use(express.json())
 
-const PORT = 3001
-
 let data = [
     { 
       "id": 1,
@@ -44,26 +42,42 @@ app.get("/api/persons", (request, response) => {
 
 app.post("/api/persons", (request, response) => {
     const newPerson = request.body
-    newPerson.id = getRandomInt(100000)
-    data = data.concat(newPerson)
-    response.status(201).json(newPerson)
+
+    if (!newPerson.name) {
+        response.status(400).json({error: "name missing"})
+    } else if (!newPerson.number) {
+        response.status(400).json({error: "number missing"})
+    } else if (data.find(person => person.name === newPerson.name)) {
+        response.status(400).json({error: "name is already taken"})
+    } else {
+        newPerson.id = getRandomInt(100000)
+        data = data.concat(newPerson)
+        response.status(201).json(newPerson)
+    }
 })
 
 app.get("/api/persons/:id", (request, response) => {
     const id = Number(request.params.id)
     const person = data.find(person => person.id === id)
-    if (person) response.json(person)
-    response.status(404).end()
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 app.delete("/api/persons/:id", (request, response) => {
     const id = Number(request.params.id)
     const person = data.find(person => person.id === id)
-    if (!person) response.status(404).end()
-    data = data.filter(person => person.id !== id)
-    response.status(204).end()
+    if (!person) {
+        response.status(404).end()
+    } else {
+        data = data.filter(person => person.id !== id)
+        response.status(204).end()
+    }
 })
 
+const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Simple server running on port ${PORT}`)
 })
