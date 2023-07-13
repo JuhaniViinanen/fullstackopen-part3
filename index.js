5d7660872e52b6a38960c12d8ab0ba7f272d91ee
Juhani Viinanen <juhani.ilmari.viinanen@gmail.com>
2023-07-13 21:02:51 +0300

    3.13 done

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+require("dotenv").config()
 const express = require("express")
 const morgan = require("morgan")
+
+const mongoose = require("mongoose")
+const dbURL = process.env.MONGODB_URI
+mongoose.connect(dbURL)
+const personSchema = new mongoose.Schema(
+    {
+        name: String,
+        number: String
+    },
+    {
+        toJSON: {
+            transform: (document, returnedObject) => {
+                returnedObject.id = returnedObject._id.toString()
+                delete returnedObject._id
+                delete returnedObject.__v
+            }
+        }
+    }
+)
+
+const Person = mongoose.model("Person", personSchema)
 
 const app = express()
 app.use(express.static("build"))
@@ -12,29 +34,6 @@ morgan.token("data", (req, res) => {
 })
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"))
 
-let data = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * max)
 }
@@ -46,7 +45,9 @@ app.get("/info", (request, response) => {
 })
 
 app.get("/api/persons", (request, response) => {
-    response.json(data)
+    Person
+      .find({})
+      .then(people => response.json(people))
 })
 
 app.post("/api/persons", (request, response) => {
@@ -86,7 +87,7 @@ app.delete("/api/persons/:id", (request, response) => {
     }
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
