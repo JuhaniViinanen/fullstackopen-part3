7c56a82da080fcf9b37fb0940900239acac80791
Juhani Viinanen <juhani.ilmari.viinanen@gmail.com>
2023-07-14 21:28:54 +0300

    3.20 done

--- models/person.js
+++ models/person.js
@@ -18,7 +18,14 @@ const personSchema = new mongoose.Schema(
           minLength: 3,
           required: true
         },
-        number: String
+        number: {
+          type: String,
+          minLength: 8,
+          validate: {
+            validator: num => /^\d{2,3}-\d*$/.test(num),
+            message: "malformed phonenumber"
+          }
+        }
     },
     {
         toJSON: {
